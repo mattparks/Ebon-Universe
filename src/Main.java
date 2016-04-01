2de654c7c4159b868384f292550962b00a23cfdf
Matthew Albrecht <mattparks5855@gmail.com>
2016-04-01 11:31:33 -0500

    Added samples to display.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import flounder.engine.*;
 
 public class Main {
 	public static void main(String[] args) {
-		FlounderEngine.init(new IModule(new MainGame(), new MainCamera(), new MainMasterRenderer()), 1080, 720, "Flounder 2.0", 144, false, true, false);
+		FlounderEngine.init(new IModule(new MainGame(), new MainCamera(), new MainMasterRenderer()), 1080, 720, "Flounder 2.0", 144, false, true, 4, false);
 		FlounderEngine.run();
 		FlounderEngine.dispose();
 	}

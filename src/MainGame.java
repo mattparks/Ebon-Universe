71c8e061b5874ef513735ac2a96cf32ee35c4bc1
Matthew Albrecht <mattparks5855@gmail.com>
2016-04-01 23:18:53 -0500

    Added buttons to contoll music.

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,15 +1,23 @@
 import flounder.devices.*;
 import flounder.engine.*;
+import flounder.inputs.*;
 import flounder.resources.*;
 import flounder.sounds.*;
 
+import static org.lwjgl.glfw.GLFW.*;
 
 public class MainGame extends IGame {
+	private KeyButton pauseMusic;
+	private KeyButton skipMusic;
+
 	public MainGame() {
 	}
 
 	@Override
 	public void init() {
+		pauseMusic = new KeyButton(GLFW_KEY_DOWN);
+		skipMusic = new KeyButton(GLFW_KEY_LEFT, GLFW_KEY_RIGHT);
+
 		Playlist playlist = new Playlist();
 		playlist.addMusic(Sound.loadSoundNow(new MyFile(DeviceSound.SOUND_FOLDER, "era-of-space.wav"), 0.5f));
 		playlist.addMusic(Sound.loadSoundNow(new MyFile(DeviceSound.SOUND_FOLDER, "spacey-ambient.wav"), 0.5f));
@@ -18,6 +26,13 @@ public class MainGame extends IGame {
 
 	@Override
 	public void update() {
+		if (pauseMusic.wasDown()) {
+			ManagerDevices.getSound().getMusicPlayer().pauseTrack();
+		}
+
+		if (skipMusic.wasDown()) {
+			ManagerDevices.getSound().getMusicPlayer().skipTrack();
+		}
 	}
 
 	@Override

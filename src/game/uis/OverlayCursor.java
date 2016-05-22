edd7a1e6e9426cedd23a651253ad60cc28099fd4
Matthew Albrecht <mattparks5855@gmail.com>
2016-05-21 22:35:50 -0500

    Better looking cursor.

--- src/game/uis/OverlayCursor.java
+++ src/game/uis/OverlayCursor.java
@@ -11,7 +11,8 @@ import java.util.*;
 public class OverlayCursor extends GuiComponent {
 	private final GuiTexture cursorPos;
 	private final Colour inactiveColour;
-	private final Colour activeColour;
+	private final Colour clickLeftColour;
+	private final Colour clickRightColour;
 	private boolean displayed;
 
 	public OverlayCursor() {
@@ -19,8 +20,9 @@ public class OverlayCursor extends GuiComponent {
 		cursorPos.getTexture().setNumberOfRows(1);
 		cursorPos.setSelectedRow(1);
 		displayed = true;
-		inactiveColour = new Colour(0.25f, 0.25f, 0.25f);
-		activeColour = new Colour(0.0f, 0.0f, 1.0f);
+		inactiveColour = new Colour(0.0f, 0.0f, 0.0f);
+		clickLeftColour = new Colour(0.0f, 0.0f, 0.8f);
+		clickRightColour = new Colour(0.8f, 0.0f, 0.0f);
 	}
 
 	public boolean isDisplayed() {
@@ -43,19 +45,27 @@ public class OverlayCursor extends GuiComponent {
 		this.inactiveColour.set(r, g, b);
 	}
 
-	public Colour getActiveColour() {
-		return activeColour;
+	public Colour getClickLeftColour() {
+		return clickLeftColour;
 	}
 
-	public void setActiveColour(final float r, final float g, final float b) {
-		this.activeColour.set(r, g, b);
+	public void setClickLeftColour(final float r, final float g, final float b) {
+		this.clickLeftColour.set(r, g, b);
+	}
+
+	public Colour getClickRightColour() {
+		return clickRightColour;
+	}
+
+	public void setClickRightColour(final float r, final float g, final float b) {
+		this.clickRightColour.set(r, g, b);
 	}
 
 	@Override
 	protected void updateSelf() {
 		if (displayed) {
 			if (ManagerDevices.getMouse().isDisplaySelected() && ManagerDevices.getDisplay().isFocused()) {
-				cursorPos.setColourOffset(GuiManager.getSelector().isLeftClick() || GuiManager.getSelector().isRightClick() ? activeColour : inactiveColour);
+				cursorPos.setColourOffset(GuiManager.getSelector().isLeftClick() ? clickLeftColour : GuiManager.getSelector().isRightClick() ? clickRightColour : inactiveColour);
 				final float averageArea = (ManagerDevices.getDisplay().getWidth() + ManagerDevices.getDisplay().getHeight()) / 2.0f;
 				cursorPos.setPosition(GuiManager.getSelector().getCursorX(), GuiManager.getSelector().getCursorY(), (33.75f / averageArea), (33.75f / averageArea) * ManagerDevices.getDisplay().getAspectRatio());
 				cursorPos.update();

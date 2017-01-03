ed00542b48e98890ce09e61f5230f2a06d9979b8
Matthew Albrecht <mattparks5855@gmail.com>
2017-01-03 06:04:16 -0600

    Updates?

--- src/ebon/entities/editing/EditorRemoveFade.java
+++ src/ebon/entities/editing/EditorRemoveFade.java
@@ -60,8 +60,8 @@ public class EditorRemoveFade extends IEditorComponent {
 		timeSlider.setPaintLabels(true);
 		panel.add(timeSlider);
 
-		// ModelObject Button.
-		JButton testButton = new JButton("ModelObject Remove");
+		// Test Button.
+		JButton testButton = new JButton("Test Remove");
 		testButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
